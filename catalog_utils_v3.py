c708be0685a10ad1258b00e41716b182a93c2909
AngelGSoto <gsoto.angel@gmail.com>
2020-09-24 19:52:37 -0300

    My notebook programs

--- catalog_utils_v3.py
+++ catalog_utils_v3.py
@@ -8,14 +8,14 @@ region_hdr_lines = [
     'fk5',
 ]
 
-def region_box_to_string(ra='5:34:40.800', dec='-5:22:43.00', text='63864', text1='47481',
+def region_box_to_string(ra='5:34:40.800', dec='-5:22:43.00', text='LL2',
                          color='yellow', width=6):
-                         pattern = 'circle({},{},{}\") # text={{{}-{}}} color={}'
-                         return pattern.format(ra, dec, width, text, text1, color)
+                         pattern = 'circle({},{},{}\") # text={{{}}} color={}'
+                         return pattern.format(ra, dec, width, text, color)
 
 
-def region_lines_from_table(table, ra='RAJ2000', dec='DEJ2000', tile='Field', name='Name',
-                             color='yellow'):
+def region_lines_from_table(table, ra='RAJ2000', dec='DEJ2000',
+                            name='Name', color='yellow'):
     """Create DS9 region lines for all sources in an astropy `table`
 
     Coordinates come from the columns named `ra` and `dec`, while
@@ -24,7 +24,8 @@ def region_lines_from_table(table, ra='RAJ2000', dec='DEJ2000', tile='Field', na
 
     region_lines = region_hdr_lines[:]
     for row in table:
-        region_lines.append(region_box_to_string(row[ra], row[dec], row[tile],
+        region_lines.append(
+                    region_box_to_string(row[ra], row[dec],
                                               row[name], color))
 
     return region_lines
